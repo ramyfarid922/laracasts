4454ee7985a5b0bc4f5f664ac5c45318f72f8b17
Ramy Farid <ramyfarid922@gmail.com>
2017-01-16 18:14:17 +0200

    Added html files that will act as the views finally returned to the browser via the controller files

---
+++ PHP_practiocioner/views/about.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="UTF-8">
+   <title>Document</title>
+   <style type="text/css">
+      header {
+     }
+ </style>
+</head>
+<body>
+   <header>
+    <h1>About Us</h1>
+  </header>
+
+</body>
+</html>
---
+++ PHP_practiocioner/views/contact.view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8">
+ <title>Document</title>
+ <style type="text/css">
+  header {
+  }
+</style>
+</head>
+<body>
+ <header>
+  <h1>
+    Contact Us  
+  </h1>
+</header>
+</body>
+</html>
