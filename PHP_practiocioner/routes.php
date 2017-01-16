3857c472f6e1f7d711200c5d4212944951496f1e
Ramy Farid <ramyfarid922@gmail.com>
2017-01-16 18:09:37 +0200

    Defined a file containing  urls and their corresponding files from the contollers folder. A routes file. It wil be loaded by the router to exploit its content in its directing method

---
+++ PHP_practiocioner/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+$router->define([
+    '' => 'controllers/index.php',
+    'about' => 'controllers/about.php',
+    'about/culture' => 'controllers/about_culture.php',
+    'contact' => 'controllers/contact.php' 
+]);
